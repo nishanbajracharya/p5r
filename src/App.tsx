234de507433ebb9fd23ef04c12bd41404bbd0984
Nishan Bajracharya <nisbaj11@gmail.com>
2025-08-15 12:44:19 +0000

    Update p5r logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from 'react';
-
 import './assets/styles/App.css';
+import logo from './assets/images/p5r-logo.png';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <img src={logo} />
     </>
   );
 }
