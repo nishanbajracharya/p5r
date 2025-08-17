dc58e54e5b75fe13683578851ed94025bcc0e527
Nishan Bajracharya <nisbaj11@gmail.com>
2025-08-17 12:42:12 +0000

    Add August data

--- src/data/months.ts
+++ src/data/months.ts
@@ -2,5 +2,6 @@ import April from './april.json';
 import May from './may.json';
 import June from './june.json';
 import July from './july.json';
+import August from './august.json';
 
-export const months = [April, May, June, July];
+export const months = [April, May, June, July, August];
